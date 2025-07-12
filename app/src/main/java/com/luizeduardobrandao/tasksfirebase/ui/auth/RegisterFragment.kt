2b977f963bd63d6343b6d73c477426317c5724db
EduardoBran <eduardo.ads1814@gmail.com>
2025-07-12 09:18:21 -0300

    criando layout para tela de registro e configurando o binding na fragment

---
+++ app/src/main/java/com/luizeduardobrandao/tasksfirebase/ui/auth/RegisterFragment.kt
@@ -0,0 +1,24 @@
+package com.luizeduardobrandao.tasksfirebase.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.luizeduardobrandao.tasksfirebase.R
+import com.luizeduardobrandao.tasksfirebase.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding: FragmentRegisterBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
