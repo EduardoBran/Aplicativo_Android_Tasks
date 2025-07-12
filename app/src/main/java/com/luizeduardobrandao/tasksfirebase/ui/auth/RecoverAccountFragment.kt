4539b43c6f414abce93b8a4a590d1e2080dbc952
EduardoBran <eduardo.ads1814@gmail.com>
2025-07-12 09:30:22 -0300

    criando layout para tela de recuperacao de conta e configurando o binding na fragment

---
+++ app/src/main/java/com/luizeduardobrandao/tasksfirebase/ui/auth/RecoverAccountFragment.kt
@@ -0,0 +1,25 @@
+package com.luizeduardobrandao.tasksfirebase.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.luizeduardobrandao.tasksfirebase.R
+import com.luizeduardobrandao.tasksfirebase.databinding.FragmentRecoverAccountBinding
+
+class RecoverAccountFragment : Fragment() {
+
+    private var _binding: FragmentRecoverAccountBinding? = null
+    private val binding: FragmentRecoverAccountBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentRecoverAccountBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
