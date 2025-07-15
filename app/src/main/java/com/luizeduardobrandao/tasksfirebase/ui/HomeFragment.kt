d8515a821a4d5505adfb8ed0032f69e0d279387c
EduardoBran <eduardo.ads1814@gmail.com>
2025-07-15 17:24:20 -0300

    implementacao para logout do usuario

--- app/src/main/java/com/luizeduardobrandao/tasksfirebase/ui/HomeFragment.kt
+++ app/src/main/java/com/luizeduardobrandao/tasksfirebase/ui/HomeFragment.kt
@@ -5,10 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.luizeduardobrandao.tasksfirebase.R
 import com.luizeduardobrandao.tasksfirebase.databinding.FragmentHomeBinding
 import com.luizeduardobrandao.tasksfirebase.ui.adapter.ViewPageAdapter
+import com.luizeduardobrandao.tasksfirebase.util.showBottomSheet
 
 // Exibe todas as tarefas
 
@@ -16,6 +22,8 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,12 +37,36 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = Firebase.auth
+
+        // Logout
+        initLogoutButton()
+
+        // TabLayout
         initTabLayout()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    // Logout do usuário
+    private fun initLogoutButton() {
+        binding.btnLogout.setOnClickListener {
+            showBottomSheet(
+                titleButton = R.string.text_button_dialog_confirm_logout,
+                titleDialog = R.string.text_title_dialog_confirm_logout,
+                message = getString(R.string.text_message_dialog_confirm_logout),
+                onClick = {
+                    auth.signOut() // desconecta do Firebase
+                    findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
+                    Toast.makeText(
+                        requireContext(), R.string.text_toast_confirm_logout, Toast.LENGTH_SHORT
+                    ).show()
+                }
+            )
+        }
     }
 
     private fun initTabLayout() {
