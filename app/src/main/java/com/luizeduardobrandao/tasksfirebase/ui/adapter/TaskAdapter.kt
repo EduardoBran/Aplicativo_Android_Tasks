89a592d1550f980501d6d6727981326b0b98916b
EduardoBran <eduardo.ads1814@gmail.com>
2025-07-15 11:19:02 -0300

    criando funcao no adapter para setar a visualizacao dos botoes de indicadores

--- app/src/main/java/com/luizeduardobrandao/tasksfirebase/ui/adapter/TaskAdapter.kt
+++ app/src/main/java/com/luizeduardobrandao/tasksfirebase/ui/adapter/TaskAdapter.kt
@@ -2,7 +2,9 @@ package com.luizeduardobrandao.tasksfirebase.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.luizeduardobrandao.tasksfirebase.data.model.Status
 import com.luizeduardobrandao.tasksfirebase.data.model.Task
 import com.luizeduardobrandao.tasksfirebase.databinding.ItemTaskBinding
 
@@ -38,5 +40,26 @@ class TaskAdapter(
 
         // Define o texto da descrição
         holder.binding.textDescription.text = task.description
+
+        // Exibir indicadores
+        setIndicators(task, holder)
+    }
+
+    // Função para exibição dos indicadores (seta de voltar e avançar)
+    private fun setIndicators(task: Task, holder: MyViewHolder) {
+        when (task.status){
+            Status.TODO -> {
+                holder.binding.btnNext.isVisible = true
+            }
+
+            Status.DOING -> {
+                holder.binding.btnBack.isVisible = true
+                holder.binding.btnNext.isVisible = true
+            }
+
+            Status.DONE -> {
+                holder.binding.btnBack.isVisible = true
+            }
+        }
     }
 }
