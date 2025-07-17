71bcd1c6b626d802b4ef072b3926d8c3fd178edd
EduardoBran <eduardo.ads1814@gmail.com>
2025-07-17 10:45:11 -0300

    recuperando e exibindo as tarefas de fazendo e feita

--- app/src/main/java/com/luizeduardobrandao/tasksfirebase/ui/DoneFragment.kt
+++ app/src/main/java/com/luizeduardobrandao/tasksfirebase/ui/DoneFragment.kt
@@ -6,12 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.luizeduardobrandao.tasksfirebase.R
 import com.luizeduardobrandao.tasksfirebase.data.model.Status
 import com.luizeduardobrandao.tasksfirebase.data.model.Task
 import com.luizeduardobrandao.tasksfirebase.databinding.FragmentDoneBinding
 import com.luizeduardobrandao.tasksfirebase.ui.adapter.TaskAdapter
+import com.luizeduardobrandao.tasksfirebase.util.FirebaseHelper
+import com.luizeduardobrandao.tasksfirebase.util.showBottomSheet
 
 // Exibe as tarefas completadas
 
@@ -39,11 +46,6 @@ class DoneFragment : Fragment() {
         getTasks()
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
     // RecyclerView
     private fun initRecyclerView(){
 
@@ -65,34 +67,123 @@ class DoneFragment : Fragment() {
     // Configuração dos eventos de cliques da RecyclerView
     private fun optionSelected(task: Task, option: Int) {
         when (option) {
+
             TaskAdapter.SELECT_REMOVE -> {
-                Toast.makeText(requireContext(), R.string.text_remove, Toast.LENGTH_SHORT).show()
+                showBottomSheet(
+                    titleDialog = R.string.text_title_dialog_confirm_remove,
+                    message = getString(R.string.text_message_dialog_confirm_remove),
+                    titleButton = R.string.text_button_dialog_confirm_logout,
+                    onClick = {
+                        deleteTask(task)
+                    }
+                )
             }
+
             TaskAdapter.SELECT_EDIT -> {
-                Toast.makeText(requireContext(), "Editando", Toast.LENGTH_SHORT).show()
+                // Envia informação do tipo Task para FormTaskFragment
+                val action = HomeFragmentDirections
+                    .actionHomeFragmentToFormTaskFragment(task)
+                findNavController().navigate(action)
             }
+
             TaskAdapter.SELECT_DETAILS -> {
                 Toast.makeText(requireContext(), "Detalhes", Toast.LENGTH_SHORT).show()
             }
+
             TaskAdapter.SELECT_BACK -> {
-                Toast.makeText(requireContext(), "Voltar", Toast.LENGTH_SHORT).show()
+                task.status = Status.DOING
+                updateTask(task)
             }
         }
     }
 
-    // Retornar a Lista de Tarefas (posteriormente será usado para recupear a lista do Firebase)
-    private fun getTasks() {
-        val taskList = listOf(
-            // Criando Lista Temporária para visualização das Tarefas
-            Task("0", "Ble ble ble", Status.DONE),
-            Task("1", "Validar informações na tela de login", Status.DONE),
-            Task("2", "Adicionar nova funcionalidade no app", Status.DONE),
-            Task("3", "Salvar token localmente", Status.DONE),
-            Task("4", "Criar funcionalidade de layout no app", Status.DONE),
-            Task("5", "Configurar Firebase", Status.DONE),
-            Task("6", "Posteriormente configurar o Room", Status.DONE)
-        )
+    // Verifica se a lista é vazia
+    private fun listEmpty(taskList: List<Task>) {
+        binding.textInfo.text = if (taskList.isEmpty()) {
+            getString(R.string.text_list_task_empty)
+        } else {
+            ""
+        }
+    }
 
-        taskAdapter.submitList(taskList)
+    // Retornar a Lista de Tarefas do Firebase
+    private fun getTasks() {
+        FirebaseHelper.getDatabase()
+            .child("tasks")
+            .child(FirebaseHelper.getIdUser())
+            .addValueEventListener(object: ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                    val taskList = mutableListOf<Task>()
+                    for (ds in snapshot.children) {
+                        val task = ds.getValue(Task::class.java) as Task
+
+                        // verificando status antes de adicionar
+                        if (task.status == Status.DONE) {
+                            taskList.add(task)
+                        }
+                    }
+
+                    // Oculta progress bar e chama função de lista vazia
+                    binding.progressBar.isVisible = false
+                    listEmpty(taskList)
+
+                    // Ordena para nova tarefa adicionada ser a primeira da lista
+                    taskList.reverse()
+
+                    // Atualiza a Lista
+                    taskAdapter.submitList(taskList)
+                }
+
+                // Cancela a busca (aplicativo fechado ou pausado)
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(
+                        requireContext(), R.string.text_generic_error, Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+    }
+
+    // Atualizando Status da Tarefa através do botão de seta
+    private fun updateTask(task: Task) {
+        FirebaseHelper.getDatabase()
+            .child("tasks")
+            .child(FirebaseHelper.getIdUser())
+            .child(task.id)
+            .setValue(task).addOnCompleteListener { result ->
+                if (result.isSuccessful){
+                    Toast.makeText(
+                        requireContext(), R.string.text_form_task_update, Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        requireContext(), R.string.text_generic_error, Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    // Deletando uma tarefa
+    private fun deleteTask(task: Task){
+        FirebaseHelper.getDatabase()
+            .child("tasks")
+            .child(FirebaseHelper.getIdUser())
+            .child(task.id)
+            .removeValue().addOnCompleteListener { result ->
+                if (result.isSuccessful){
+                    Toast.makeText(
+                        requireContext(), R.string.text_remove, Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        requireContext(), R.string.text_generic_error, Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
